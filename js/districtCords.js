a40669d63dd82cd5c66d4ed946ecd32ea6af48aa
Marcos dos Santos Carvalho <marcos.carvalho@ezoom.com.br>
2024-11-14 11:47:34 -0300

    refactor: usage of early returns

--- js/districtCords.js
+++ js/districtCords.js
@@ -9,30 +9,34 @@ async function getDistrictCoordinates(districtName, cityName) {
         'User-Agent': 'YourAppName', // Include a user-agent header
       }
     });
+
     const data = await response.json();
 
-    if (data && data.length > 0) {
-      // Get the first result that matches the search
-      const { geojson, display_name } = data[0];
-
-      // Check if GeoJSON data is available
-      if (geojson && geojson.type === 'Polygon') {
-        // Extract the coordinates (this will be an array of arrays of [lng, lat] pairs)
-        const coordinates = geojson.coordinates[0].map(coord => ({
-          lat: coord[1],
-          lng: coord[0]
-        }));
-
-        console.log(`Coordinates for ${display_name}:`, coordinates);
-        return coordinates;
-      } else {
-        console.error('No polygon data available for the specified district.');
-        return null;
-      }
-    } else {
+    // Get the first result that matches the search
+    if (!data || data.length < 0) {
       console.error('No results found for the specified district.');
       return null;
     }
+
+    const { geojson, display_name } = data[0];
+
+    // Check if GeoJSON data is available
+    const polygonGeoCoords = geojson && geojson.type === 'Polygon';
+
+    if (!polygonGeoCoords) {
+      console.error('No polygon data available for the specified district.');
+      return null;
+    }
+
+    // Extract the coordinates (this will be an array of arrays of [lng, lat] pairs)
+    const coordinates = geojson.coordinates[0].map(coord => ({
+      lat: coord[1],
+      lng: coord[0]
+    }));
+
+    console.log(`Coordinates for ${display_name}:`, coordinates);
+    return coordinates;
+
   } catch (error) {
     console.error('Error fetching coordinates:', error);
     return null;
